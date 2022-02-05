f699a67d2e7b4c76dd9a0a4f7dcb5bf04f441f6a
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-06 00:30:44 +0600

    Data Type discuss

---
+++ src/Class2/FirstProgram.java
@@ -0,0 +1,29 @@
+package Class2;
+
+public class FirstProgram {
+    public static void main(String[] args) { // Must Write this method for run this program
+        //Data Type.....
+        int a;  // primitive data type start
+        a=776458765; // total 9 number can give
+        float b= 1795.8745467346573f;// must give F\f and this print total 8 number
+        double c;
+        c = 15.4674857346573847436574645637465374657; //total 17 number can give.
+        long d= 1873465735675678123L;// Less than or equal 19 number
+        byte e=12;
+        boolean f=true;
+        char g='A';
+        short h=546; // primitive data type end
+
+        String i="Mahamud is a Instructor of java"; // non-primitive data type
+
+
+        System.out.printf(a+"%n"+b+"%n"); //System.out.printf("%n")---> print new line
+        System.out.println(c+"\n");//System.out.println("\n")---> print new line
+        System.out.println(d+"\n"+e+"\n"+ f +"\n"+g+"\n"+h+"\n"+i);
+
+
+    }
+
+
+
+}
