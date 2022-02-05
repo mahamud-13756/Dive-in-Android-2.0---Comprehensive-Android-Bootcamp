faf3b054d47d991e9c3f6e928df271b4a73971cf
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-06 01:57:52 +0600

    switch case . break deini tai 2==2

---
+++ src/Switch.java
@@ -0,0 +1,14 @@
+public class Switch {
+    public static void main(String[] args) {
+        int a=2;
+        switch(a) {
+            case 1:
+                System.out.println("1==1");
+            case 2:
+                System.out.println("2==2");
+            default:
+                System.out.println("Out of range");
+
+        }
+    }
+}
