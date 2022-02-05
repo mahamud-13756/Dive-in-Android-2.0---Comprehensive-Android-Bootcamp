c4a9b6b4a09c1562dd2b7c8aaa300723ebf720ec
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-06 00:53:24 +0600

    if-else statement

---
+++ src/Class2/IfElse.java
@@ -0,0 +1,25 @@
+package Class2;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        int a=sc.nextInt();
+        System.out.println("Please enter another number");
+        int b=sc.nextInt();
+
+        if (a>b){
+            System.out.println("The big number is: "+a);
+        }
+        else {
+            System.out.println("The big number is: "+b);
+        }
+
+    }
+
+
+}
