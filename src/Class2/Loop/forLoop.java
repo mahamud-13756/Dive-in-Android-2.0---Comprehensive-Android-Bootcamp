5ab50f6e8ce70f36050148813520d135e8c57cfc
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-06 02:11:01 +0600

    for loop

---
+++ src/Class2/Loop/forLoop.java
@@ -0,0 +1,12 @@
+package Class2.Loop;
+
+public class forLoop {
+    public static void main(String[] args) {
+        //int i;
+        for(int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+}
