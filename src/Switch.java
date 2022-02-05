397a88960b5c64ac01372561996ad662fbc33315
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-06 02:00:14 +0600

    now this code is correct

--- src/Switch.java
+++ src/Switch.java
@@ -1,11 +1,13 @@
 public class Switch {
     public static void main(String[] args) {
-        int a=2;
+        int a=3;
         switch(a) {
             case 1:
                 System.out.println("1==1");
+                break;
             case 2:
                 System.out.println("2==2");
+                break;
             default:
                 System.out.println("Out of range");
 
