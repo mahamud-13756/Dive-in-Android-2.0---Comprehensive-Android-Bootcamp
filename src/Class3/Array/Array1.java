6491fd7ac95ba5eb5f2546011b25260b335baaf6
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-07 20:55:51 +0600

    this is another way to print

--- src/Class3/Array/Array1.java
+++ src/Class3/Array/Array1.java
@@ -6,16 +6,20 @@ public class Array1 {
 
        // int a[]={10,20,30,40,50};
         int[] a={10,20,30,40,50};
-        System.out.println(a[0]);
-        System.out.println(a[1]);
-        System.out.println(a[2]);
-        System.out.println(a[3]);
-        System.out.println(a[4]);
+        for (int i=0;i<a.length;i++){
+        System.out.println(a[i]);}
+//        System.out.println(a[1]);
+//        System.out.println(a[2]);
+//        System.out.println(a[3]);
+//        System.out.println(a[4]);
 
         String[] b={"Mahamud","Nazmul","Kawser"};
-        System.out.println(b[0]);
-        System.out.println(b[1]);
-        System.out.println(b[2]);
+        for (String g : b) {
+            System.out.println(g);
+        }
+//        System.out.println(b[0]);
+//        System.out.println(b[1]);
+//        System.out.println(b[2]);
 //        System.out.println(b[3]);
 //        System.out.println(b[4]);
 
