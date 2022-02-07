2927bb6efca1ff612d8fe0625a9eb0489a77cd87
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-07 20:47:47 +0600

    Array

---
+++ src/Class3/Array/Array1.java
@@ -0,0 +1,25 @@
+package Class3.Array;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+
+       // int a[]={10,20,30,40,50};
+        int[] a={10,20,30,40,50};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+        String[] b={"Mahamud","Nazmul","Kawser"};
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+//        System.out.println(b[3]);
+//        System.out.println(b[4]);
+
+
+
+    }
+}
